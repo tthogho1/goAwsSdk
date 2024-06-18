ec54b770ef1448f0c187b8c5eb5a2b11f8d580fb
t-tougou <tthogho1@gmail.com>
2024-06-18 21:45:55 +0900

    use ListObjectsV2 for download from s3

--- utils/s3.go
+++ utils/s3.go
@@ -13,35 +13,48 @@ import (
 )
 
 func Download(svc *s3.S3, bucket string, localdir string) {
-	resp, err := svc.ListObjects(&s3.ListObjectsInput{Bucket: aws.String(bucket)})
+	var continuationToken *string
+	downloader := s3manager.NewDownloaderWithClient(svc)
+
+	for {
+		resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{
+			Bucket:            aws.String(bucket),
+			ContinuationToken: continuationToken,
+		})
+		if err != nil {
+			fmt.Println("failed to list objects", err)
+			return
+		}
+
+		for _, obj := range resp.Contents {
+			fmt.Println("Downloading", *obj.Key)
+
+			downloadObject(downloader, bucket, *obj.Key, localdir)
+		}
+
+		if !aws.BoolValue(resp.IsTruncated) {
+			break
+		}
+		continuationToken = resp.NextContinuationToken
+	}
+
+}
+
+func downloadObject(downloader *s3manager.Downloader, bucket string, key string, localdir string) {
+	localFilePath := filepath.Join(localdir, key)
+	file, err := os.Create(localFilePath)
 	if err != nil {
-		fmt.Println("failed to list objects", err)
+		fmt.Println("failed to create file", err)
 		return
 	}
+	defer file.Close()
 
-	for _, item := range resp.Contents {
-		fmt.Println(*item.Key)
-
-		localFilePath := filepath.Join(localdir, *item.Key)
-		file, err := os.Create(localFilePath)
-		if err != nil {
-			fmt.Println("failed to create file", err)
-			return
-		}
-		defer file.Close()
-
-		downloader := s3manager.NewDownloaderWithClient(svc)
-		_, err2 := downloader.Download(file, &s3.GetObjectInput{
-			Bucket: aws.String(bucket),
-			Key:    item.Key,
-		})
-
-		if err2 != nil {
-			fmt.Println("failed to get object", err)
-			return
-		}
-
-		fmt.Println("downloaded", localFilePath)
+	_, err2 := downloader.Download(file, &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err2 != nil {
+		fmt.Println("failed to get object", err)
+		return
 	}
-
 }
