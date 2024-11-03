bc3c3c94935f845635e28b616656262dafaeccbc
t-tougou <tthogho1@gmail.com>
2024-11-03 22:01:33 +0900

    Add Function : Execute Command on EC2

---
+++ AWSCost.bat
@@ -0,0 +1,19 @@
+rem awsctrl -profile default -c describe -t EC2
+
+rem awsctrl -profile default -c describe -t EC2
+rem awsctrl -h help
+
+rem aws起動
+
+rem awsctrl -profile default -c up -t EC2 -i i-0b38a88b8f5558fd2
+
+
+rem awsctrl -profile default -c describe -t EC2 -p B2C
+
+rem awsctrl -profile default -c describe -t EC2 
+
+
+awsctrl -profile myregion -c cost -start 2024-09-01  -end 2024-09-18s
+
+
+pause
